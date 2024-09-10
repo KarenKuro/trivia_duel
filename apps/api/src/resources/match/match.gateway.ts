0764b3508d607628066d76e2eaae1e42ede38a9c
KarenKuro <kkarenk888@gmail.com>
2024-09-10 17:04:35 +0400

    remove console.logs

--- apps/api/src/resources/match/match.gateway.ts
+++ apps/api/src/resources/match/match.gateway.ts
@@ -39,7 +39,6 @@ export class MatchGateway implements OnGatewayConnection, OnGatewayDisconnect {
       client.join(userId.toString());
 
       this.clients.add(client);
-      console.log(`Client connected: ${client?.id}`);
     } catch (error) {
       console.log('socket error', error);
       client.disconnect();

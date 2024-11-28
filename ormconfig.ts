43d2034ee10b8ffc3a42e9e49ceef26c177edc3e
KarenKuro <kkarenk888@gmail.com>
2024-11-29 01:03:27 +0400

    ft/ add compose folder

--- ormconfig.ts
+++ ormconfig.ts
@@ -24,7 +24,7 @@ const dbConfig: MysqlConnectionOptions = {
   username: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
-  entities: [__dirname + '/**/**/entities/**.entity.ts'],
+  entities: [__dirname + '/**/**/entities/**.entity{.ts,.js}'],
   migrationsTableName: 'app_migrations',
   migrations: [__dirname + '**/**/migrations/*.ts'],
   bigNumberStrings: false,
